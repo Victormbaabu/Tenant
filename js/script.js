156cadca9d4d2e18b313020fa3ef522eb6526f75
Victormbaabu <victor.mbaabu@strathmore.edu>
2021-12-01 13:06:16 +0300

    add my files

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+  /*studio apartment toggle function*/
     $("#haoA").click(function() {
       $("#haoA").slideToggle();
       $("#pA").slideToggle();
@@ -8,7 +9,37 @@ $(document).ready(function(){
       $("#pA").slideToggle();
       $("#haoA").slideToggle();
     }); 
+  /*studio apartment sitting room description toggle function*/
+    $("#hao1").click(function() {
+      $("#hao1").slideToggle();
+      $("#p1").slideToggle();
+     });
 
+    $("#p1").click(function() {
+      $("#p1").slideToggle();
+      $("#hao1").slideToggle();
+    }); 
+/*studio apartment kitchen description toggle function*/
+    $("#hao2").click(function() {
+      $("#hao2").slideToggle();
+      $("#p2").slideToggle();
+     });
+
+    $("#p2").click(function() {
+      $("#p2").slideToggle();
+      $("#hao2").slideToggle();
+    }); 
+
+/*studio apartment bedroom description toggle function*/
+    $("#hao3").click(function() {
+      $("#hao3").slideToggle();
+      $("#p3").slideToggle();
+     });
+
+    $("#p3").click(function() {
+      $("#p3").slideToggle();
+      $("#hao3").slideToggle();
+    });     
 
     $("#haoB").click(function() {
         $("#haoB").slideToggle();
@@ -20,7 +51,7 @@ $(document).ready(function(){
         $("#haoB").slideToggle();
      });
 
-    
+  /*1 bedroom apartment toggle function*/  
      $("#haoAA").click(function() {
       $("#haoAA").slideToggle();
       $("#pAA").slideToggle();
@@ -30,6 +61,16 @@ $(document).ready(function(){
       $("#pAA").slideToggle();
       $("#haoAA").slideToggle();
     }); 
+
+    $("#hao1a").click(function() {
+      $("#hao1a").slideToggle();
+      $("#p11").slideToggle();
+      });
+
+   $("#p1a").click(function() {
+      $("#p1a").slideToggle();
+      $("#hao1a").slideToggle();
+   });
     
     $("#haoBB").click(function() {
       $("#haoBB").slideToggle();
@@ -40,6 +81,8 @@ $(document).ready(function(){
       $("#pBB").slideToggle();
       $("#haoBB").slideToggle();
     }); 
+
+   
 
 });
 
