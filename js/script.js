d668dbe401541afa5ec8ac0f39c5173b20a7b625
Victormbaabu <victor.mbaabu@strathmore.edu>
2021-12-01 14:34:48 +0300

    Added functions for toggle

--- js/script.js
+++ js/script.js
@@ -1,77 +1,35 @@
 $(document).ready(function(){
-  /*studio apartment toggle function*/
-    $("#haoA").click(function() {
-      $("#haoA").slideToggle();
-      $("#pA").slideToggle();
-     });
 
-    $("#pA").click(function() {
-      $("#pA").slideToggle();
-      $("#haoA").slideToggle();
-    }); 
-  /*studio apartment sitting room description toggle function*/
-    $("#hao1").click(function() {
-      $("#hao1").slideToggle();
-      $("#p1").slideToggle();
+  function toggleFun (showElementId, hideElementId){
+    $(showElementId).click(function() {
+      $(showElementId).slideToggle();
+      $(hideElementId).slideToggle();
      });
-
-    $("#p1").click(function() {
-      $("#p1").slideToggle();
-      $("#hao1").slideToggle();
-    }); 
+  }
+/*studio apartment toggle function*/
+  toggleFun("#haoA", "#pA");
+  toggleFun("#pA", "#haoA");
+/*studio apartment sitting room description toggle function*/
+  toggleFun("#hao1", "#p1");
+  toggleFun("#p1", "#hao1");
 /*studio apartment kitchen description toggle function*/
-    $("#hao2").click(function() {
-      $("#hao2").slideToggle();
-      $("#p2").slideToggle();
-     });
-
-    $("#p2").click(function() {
-      $("#p2").slideToggle();
-      $("#hao2").slideToggle();
-    }); 
-
+  toggleFun("#hao2", "#p2");
+  toggleFun("#p2", "#hao2");
 /*studio apartment bedroom description toggle function*/
-    $("#hao3").click(function() {
-      $("#hao3").slideToggle();
-      $("#p3").slideToggle();
-     });
+  toggleFun("#hao3", "#p3");
+  toggleFun("#p3", "#hao3");
+/*studio apartment row two toggle function*/
+toggleFun("#haoB", "#pB");
+toggleFun("#pB", "#haoB"); 
+ /*1 bedroom apartment toggle function*/  
+ toggleFun("#haoAA", "#pAA");
+ toggleFun("#pAA", "#haoAA");
 
-    $("#p3").click(function() {
-      $("#p3").slideToggle();
-      $("#hao3").slideToggle();
-    });     
+ /*1 bedroom apartment sitting room toggle function*/ 
+  toggleFun("#hao1a", "#p1a");
+ toggleFun("#p1a", "#hao1a");
 
-    $("#haoB").click(function() {
-        $("#haoB").slideToggle();
-        $("#pB").slideToggle();
-        });
-
-     $("#pB").click(function() {
-        $("#pB").slideToggle();
-        $("#haoB").slideToggle();
-     });
-
-  /*1 bedroom apartment toggle function*/  
-     $("#haoAA").click(function() {
-      $("#haoAA").slideToggle();
-      $("#pAA").slideToggle();
-    });
-
-    $("#pAA").click(function() {
-      $("#pAA").slideToggle();
-      $("#haoAA").slideToggle();
-    }); 
-
-    $("#hao1a").click(function() {
-      $("#hao1a").slideToggle();
-      $("#p11").slideToggle();
-      });
-
-   $("#p1a").click(function() {
-      $("#p1a").slideToggle();
-      $("#hao1a").slideToggle();
-   });
-    
+      
     $("#haoBB").click(function() {
       $("#haoBB").slideToggle();
       $("#pBB").slideToggle();
